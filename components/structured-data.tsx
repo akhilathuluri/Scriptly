3a4d1a9093c42d01e54e2227acad05def626c081
Akhil Athuluri <89147384+akhilathuluri@users.noreply.github.com>
2025-10-25 17:58:15 +0530

    Add files via upload

---
+++ components/structured-data.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import Script from 'next/script';
+
+interface StructuredDataProps {
+  data: object | object[];
+}
+
+export function StructuredData({ data }: StructuredDataProps) {
+  const schemas = Array.isArray(data) ? data : [data];
+  
+  return (
+    <>
+      {schemas.map((schema, index) => (
+        <Script
+          key={index}
+          id={`structured-data-${index}`}
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+        />
+      ))}
+    </>
+  );
+}
